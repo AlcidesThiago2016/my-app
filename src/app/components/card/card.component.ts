0d590235f9949129068c79663ae4afa1ea31c959
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2023-10-10 15:21:49 -0300

    create pokemon service

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  name:string = "BULBASSAUR"
+  attributesTypes:string[] = ['FIRE', 'ROCK']
+
+  constructor(){}
+  ngOnInit(): void {
+  }
 
 }
